a27280a646cff60f80f1768c5811ecb1f88d56a5
ali rahimi <ali.rahimi@divar.ir>
2024-01-11 20:07:17 +0330

    Add NOTIFICATION_ID and NOTIFICATION_CHANNEL_ID

---
+++ app/src/main/java/com/alixa/spotifycloneyt/other/Constants.kt
@@ -0,0 +1,9 @@
+package com.alixa.spotifycloneyt.other
+
+object Constants {
+    const val SONG_COLLECTION = "songs"
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
+
+}
